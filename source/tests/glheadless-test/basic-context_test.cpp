0899f270a9262d89feb812430449c28bcd869a58
Jan Ole Vollmer <jan.vollmer@hpi.de>
2016-09-13 15:43:47 +0200

    Add basic tests

---
+++ source/tests/glheadless-test/basic-context_test.cpp
@@ -0,0 +1,100 @@
+#if WIN32
+#include <Windows.h>
+#include <gl/GL.h>
+#endif
+
+#include <gmock/gmock.h>
+
+#include <glheadless/Context.h>
+
+
+using namespace glheadless;
+
+
+class BasicContext_Test : public testing::Test {
+    
+};
+
+
+TEST_F(BasicContext_Test, Initialization) {
+    Context context;
+
+    EXPECT_FALSE(context.valid());
+    EXPECT_FALSE(context.lastErrorCode());
+}
+
+
+TEST_F(BasicContext_Test, Create32) {
+    Context context;
+
+    context.setVersion(3, 2);
+    context.create();
+    EXPECT_TRUE(context.valid());
+    EXPECT_FALSE(context.lastErrorCode());
+}
+
+
+TEST_F(BasicContext_Test, CreateDefault) {
+    Context context;
+
+    context.create();
+    EXPECT_TRUE(context.valid());
+    EXPECT_FALSE(context.lastErrorCode());
+}
+
+
+TEST_F(BasicContext_Test, Destroy) {
+    Context context;
+
+    context.create();
+    ASSERT_TRUE(context.valid());
+    ASSERT_FALSE(context.lastErrorCode());
+
+    context.destroy();
+    EXPECT_FALSE(context.valid());
+    EXPECT_FALSE(context.lastErrorCode());
+}
+
+
+TEST_F(BasicContext_Test, MakeCurrent) {
+    Context context;
+
+    context.create();
+    ASSERT_TRUE(context.valid());
+
+    const auto success = context.makeCurrent();
+    EXPECT_TRUE(success);
+    EXPECT_FALSE(context.lastErrorCode());
+}
+
+
+TEST_F(BasicContext_Test, DoneCurrent) {
+    Context context;
+
+    context.create();
+    ASSERT_TRUE(context.valid());
+
+    auto success = context.makeCurrent();
+    ASSERT_TRUE(success);
+
+    success = context.doneCurrent();
+    EXPECT_TRUE(success);
+    EXPECT_FALSE(context.lastErrorCode());
+}
+
+
+TEST_F(BasicContext_Test, GetVersion) {
+    Context context;
+
+    context.create();
+    ASSERT_TRUE(context.valid());
+
+    auto success = context.makeCurrent();
+    ASSERT_TRUE(success);
+
+    const auto versionStringRaw = glGetString(GL_VERSION);
+    ASSERT_NE(nullptr, versionStringRaw);
+
+    const auto versionString = std::string(reinterpret_cast<const char*>(versionStringRaw));
+    EXPECT_NE(0, versionString.size());
+}
